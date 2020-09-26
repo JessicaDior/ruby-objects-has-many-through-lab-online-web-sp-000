d5634dd3006425b93acf8ae270cfd3e50ffe53a2
Jc Dior <jessicaclairdior@gmail.com>
2020-09-26 18:35:54 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient
+  attr_accessor :name, :doctor, :appointment
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment
+  end
+
+  def appointments
+  end
+
+  def doctors
+  end
+
+end
